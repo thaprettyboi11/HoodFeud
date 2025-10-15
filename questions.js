de13b723b62192b87154311aa5fe7d658e501c29
thaprettyboi11 <thaprettyboi@gmail.com>
2025-10-14 23:13:59 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,11 @@
+{
+  type: "main",
+  question: "Name something you bring to a cookout",
+  answers: [
+    { text: "Hot dogs", points: 30 },
+    { text: "Chips", points: 25 },
+    { text: "Drinks", points: 20 },
+    { text: "Plates", points: 15 },
+    { text: "Napkins", points: 10 }
+  ]
+}
